8aca8690ccc989bf79602c85c36ee22bfeb02188
joe-yama <s1r0mqme@gmail.com>
2024-10-22 23:47:59 +0900

    Considering eval order between % and *

--- Review02.java
+++ Review02.java
@@ -1,7 +1,7 @@
 public class Review02 {
   public static void main(String[] args) {
     for (int i = 1; i <= 100; ++i) {
-      if (i % 3 * 5 == 0) {
+      if (i % (3 * 5) == 0) {
         System.out.println("FizzBuzz");
       } else if (i % 3 == 0) {
         System.out.println("Fizz");
